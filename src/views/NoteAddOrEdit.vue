d6829e0ce87d05df90b1b337dd81b088dad01941
Taras Yemtsov <flamytwista@gmail.com>
2020-02-11 17:05:43 +0300

    add-edit more markuping

--- src/views/NoteAddOrEdit.vue
+++ src/views/NoteAddOrEdit.vue
@@ -1,7 +1,18 @@
 <template>
   <container class="note-edit">
     <h1>{{ title }}</h1>
-    <txt placeholder="Заголовок"></txt>
+    <txt class="note-edit__input-title" placeholder="Заголовок"></txt>
+
+    <btn class="note-edit__btn-add-task">
+      Добавить Задачу
+    </btn>
+    <div class="clearfix"></div>
+    <div class="note-edit__control-buttons">
+      <btn class="">{{ actionTitle }}</btn>
+      <btn class="btn--error">Отменить</btn>
+      <btn class="btn--error">Удалить</btn>
+    </div>
+
   </container>
 </template>
 
@@ -18,6 +29,9 @@ export default {
     },
     title () {
       return (this.isEditing ? 'Редактирование' : 'Добавление') + ' заметки'
+    },
+    actionTitle () {
+      return this.isEditing ? 'Изменить' : 'Добавить'
     }
   }
 
@@ -26,6 +40,26 @@ export default {
 
 <style scoped lang="scss">
   .note-edit {
-    zoom: 1;
+    .note-edit__input-title {
+      margin-bottom: $gap;
+    }
+    .note-edit__btn-add-task {
+      margin-bottom: $gap * 2;
+      float: right;
+    }
+
+    .note-edit__control-buttons {
+      display: flex;
+      flex-flow: column wrap;
+      justify-content: flex-end;
+      align-items: flex-end;
+      @media (min-width: $bpSm2) {
+        flex-flow: row wrap;
+      }
+      > * {
+        margin-left: $gap;
+        margin-bottom: $gap;
+      }
+    }
   }
 </style>
